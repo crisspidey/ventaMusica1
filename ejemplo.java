0e716e0aeb625bfc8e8bc2d1cdbb2cc79152bfef
cris <crisspidey8@gmail.com>
2018-04-25 21:06:25 -0500

    cambios

--- ejemplo.java
+++ ejemplo.java
@@ -11,5 +11,9 @@ public class ejemplo {
         System.out.println("Hola cris, como estás");
         System.out.println("casa mia");
         System.out.println("casa mia");
+        
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
 }
